a28ac33e05e4ed7ecc5ad4a3be441d206cb5b138
Muktar SayedSaleh <muktar@monjz.com>
2023-07-03 00:53:17 +0300

    UPDATED: Selenium new version deprecated chrome_options

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from linkedineasyapply import LinkedinEasyApply
 
 
 def init_browser():
-    browser_options = Options()
-    options = [
-        "--disable-blink-features",
-        "--no-sandbox",
-        "--start-maximized",
-        "--disable-extensions",
-        "--ignore-certificate-errors",
-        "--disable-blink-features=AutomationControlled",
-    ]
-
-    for option in options:
-        browser_options.add_argument(option)
+    browser_options = webdriver.ChromeOptions()
+    
+    browser_options.add_argument("--disable-blink-features")
+    browser_options.add_argument("--no-sandbox")
+    browser_options.add_argument("--start-maximized")
+    browser_options.add_argument("--disable-extensions")
+    browser_options.add_argument("--ignore-certificate-errors")
+    browser_options.add_argument("--disable-blink-features=AutomationControlled")
 
     driver = webdriver.Chrome(
-        ChromeDriverManager().install(), chrome_options=browser_options
+        # service=ChromeDriverManager().install(),
+        options=browser_options
     )
-
+    
     driver.set_window_position(0, 0)
     driver.maximize_window()
 
